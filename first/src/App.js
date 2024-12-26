e7d61c8efc207a593674c5aecb8f4a10f1699fa9
Ekkospace <xbw112233@gmail.com>
2024-12-26 21:14:41 +1100

    refactor App component to utilize useState for state management and improve event handling

--- first/src/App.js
+++ first/src/App.js
@@ -1,4 +1,9 @@
 
+//在react中，状态被认为是只读的，只能通过setState()方法来修改状态(而不是count++)，而不能直接修改状态，而是直接替换他们。
+//直接替换状态会导致组件不会重新渲染(引发视图的更新)，因为react无法检测到状态的变化。
+
+import {useState}from 'react';
+
 const count = 100;
 const list=[
   {id:1,name:'John'},
@@ -8,6 +13,19 @@ const list=[
 const isLogin = false; 
 
 const articleType= 0;
+
+//箭头函数与function组件 组件开头首字母需要大写
+//箭头函数
+const Button = () => {
+  return <button>Click me!!!</button>
+}
+
+
+// {/* <defineButton /> */}
+// function Button() {
+// //业务 逻辑组件
+//   return <button>Click me!!!</button>
+// }
 
 
 function getArticleTem() {
@@ -32,12 +50,35 @@ function App() {
   // {/**Event handling name*/}
   // const handleClick = (name) => {console.log('clicked',name)};
 
-  {/**Event handling name and e*/}
-  const handleClick = (name, e) => {console.log('clicked',name, e)};
+  // {/**Event handling name and e*/}
+  // const handleClick = (name, e) => {console.log('clicked',name, e)};
+  
+  //1.10 Use state
+  const [count, setCount] = useState(0);
+  //1.10.1 Event handling 点击事件回调
+  const handleClick = () => {
+    //作用：用传入的新值修改count 和 重新使用新的count值渲染ui
+    setCount(count + 2)};
+  //1.10.2 修改对象状态
+  const [form, setForm] = useState({name: 'Ekko'});
+
+  const changeForm = () => {  
+    // 错误写法form.name = 'Jack';
+    //正确写法
+    setForm({
+      name: 'Jack'
+  }
+  )}
+  
   
   return (
     <div className="App">
-      this is app
+      {/**Using component 自闭和*/}
+      <Button />
+      {/**Using component 成对标签*/}
+      <Button></Button>
+
+
       {/** Passing strings using quotes */}
       {'this is message'}
       {/** Passing numbers */}
@@ -67,6 +108,17 @@ function App() {
         <div className= "App">
         <button onClick={(e) => handleClick('ekko',e)}>Click me</button>
         </div>
+
+        {/* //1.10 Use state   
+        // UseState是一个hook函数，返回值是一个数组
+        // 数组中的第一个参数是状态变量，第二个是set函数用来修改状态变量
+        // UseState的参数将作为count的初始值*/}
+        <button onClick={handleClick}>{count}</button>
+        <button onClick={changeForm}>修改form{form.name}</button>
+
+        
+
+
    
 
 
@@ -75,5 +127,8 @@ function App() {
     
   );
 }
+
+
+
 
 export default App;
