58747870a9d44bb061ce3705645a1b24d2d04fc2
Ekkospace <xbw112233@gmail.com>
2024-12-26 12:50:05 +1100

    update dependencies and enhance App component with dynamic rendering

--- first/src/App.js
+++ first/src/App.js
@@ -1,9 +1,41 @@
 
+const count = 100;
+const list=[
+  {id:1,name:'John'},
+  {id:2,name:'Doe'},
+  {id:3,name:'Smith'},
+]
+
+
+function getnName() {
+  return 'John Doe';
+}
+
 function App() {
   return (
     <div className="App">
       this is app
+      {/** Passing strings using quotes */}
+      {'this is message'}
+      {/** Passing numbers */}
+      {1}
+      {/** Passing boolean */}
+      {count}
+      {/** Passing function */}
+      {getnName()}
+      {/** Passing array */}
+      <div>
+      {new Date().getFullYear()}
+      </div>
+      <div style={{ color: 'red' }}>this is red</div>
+
+      <ul>
+        {list.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
+    
   );
 }
 
